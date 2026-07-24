9bca8984e2c6424d9a0cd53e892b63e62769739a
Abdulkabir Musa <musaabdulkabeer19@gmail.com>
2026-07-24 21:52:13 +0100

    fix(cask): update SHA256 hashes for re-built v0.1.0 release

--- Casks/ithqan.rb
+++ Casks/ithqan.rb
@@ -2,8 +2,8 @@ cask "ithqan" do
   arch arm: "aarch64", intel: "x64"
 
   version "0.1.0"
-  sha256 arm:   "1713cee39d3c108ec4441f2d363138fba8642ae1d077dee911ab027a7dabb9e4",
-         intel: "5739b5e22006c515bddc3d368f042c49abf644d78258b3aaf2b6a49d2276030d"
+  sha256 arm:   "bfd8b6c7d4d4bf39e5dd6a9099a65489977224f29d49370d27ceb456ee42f924",
+         intel: "51a56c4489550a53926d6d415dc9def9eb3b983324b6c29d48aabf5dcda8ed37"
 
   url "https://github.com/AIEraDev/Ithqan/releases/download/v#{version}/Ithqan_v#{version}_macos_#{arch}.dmg",
       verified: "github.com/AIEraDev/Ithqan/"
